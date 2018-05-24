dad8e8a2d364712ae2146f410fd778306694524a
Antoine <RandleAntoine@gmail.com>
2018-05-24 14:55:54 -0700

    adding players to game

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
+var player1 = {
+    name: "PLAYER 1",
+    score: 0,
+    sequence: []
+}
+
+var player2 = {
+    name: "PLAYER 2",
+    score: 0,
+    sequence: []
+}
+
+var currentPlayer = player1
+
+function switchTurns() {
+    if(currentPlayer === player1) {
+        currentPlayer = player2
+    } else {
+        // compare player1.score and player2.score and announce the winner
+        if(player1.score > player2.score) {
+            alert("You've won player 1 :)")
+        } else if(player2.score > player1.score) {
+            alert("You've won player 2 :)")
+        } else {
+            alert("Tie game!")
+        }
+    }
+}
+
 var Game=(function(){
     var faces=$("#gameContent .circle");
     var sequence=[];
     var userSequence=[];
+
+    
 
  // Click button to start the game.   
     function init(){
